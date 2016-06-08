b3b4f2225f65b69fe4f79db857dd2168fc372a10
MilkshakePanda <iam.superpeluche@gmail.com>
2016-06-08 12:59:42 +0100

    Extend Array prototype with randIndex

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,15 @@ const usernameInput = document.querySelector(".username-input")
 const messageInput  = document.querySelector(".message-input")
 const loginPage     = document.querySelector(".login")
 const chatPage     = document.querySelector(".chat")
+const colors       = ["#BD1E1E", "#EFCB68", "#558D8F", "#D79C57", "#B9314F", "#4392F1", "#18206F"]
 let username
+
+Array.prototype.randIndex = function() {
+
+    var ri = Math.floor(Math.random() * this.length)
+    return this[ri] 
+
+}
 
 // On open
 connection.onopen = () => console.log("connected") 
@@ -31,9 +39,21 @@ const sendMessage = () => {
 
 const displayMessage = (message) => {
     const incomingMessage = JSON.parse(message)
-    const messageElement = document.createElement("P") // create an html element
-    messageElement.innerHTML = incomingMessage.user + " says: " + incomingMessage.body 
-    chatContainer.appendChild(messageElement)        // append the fucker to the container
+    const messageElement = document.createElement("P") 
+    const usernameSpan   = document.createElement("span") 
+    const messageBodySpan = document.createElement("span")
+    
+    usernameSpan.classList.add("username")
+    usernameSpan.style.color = "mediumseagreen" 
+    usernameSpan.innerHTML = incomingMessage.user  + ": "
+    
+    messageBodySpan.classList.add("messageBody")
+    messageBodySpan.innerHTML = incomingMessage.body
+    
+    messageElement.appendChild(usernameSpan)
+    messageElement.appendChild(messageBodySpan)
+    
+    chatContainer.appendChild(messageElement)
 }
 
 const setUsername = () => {

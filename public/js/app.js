c6c0281f9eca0ee41dae20a5712a775435bee563
MilkshakePanda <iam.superpeluche@gmail.com>
2016-06-09 17:12:55 +0100

    Add notes above functions and quick refactor of the code

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,9 @@ connection.onmessage = (event) => {
     const incomingData = JSON.parse(event.data)
     const eventAction  = incomingData.action
     
-    // Switch case
+    // Here we check the action event sent by the server
+    // depending on the action, we perform different tasks
+    // Ex: if chat message then display message but if new user then notifyChatRoom and updateConnectedUsers
     switch(eventAction) {
 
         case "new message":
@@ -27,87 +29,94 @@ connection.onmessage = (event) => {
             break;
         case "user joined":
             notifyUsers(incomingData) 
-            showConnectedUsers(incomingData.usernames)
+            updateConnectedUsers(incomingData.usernames)
             break;
         case "user left":
             notifyUsers(incomingData)
-            showConnectedUsers(incomingData.usernames)
+            updateConnectedUsers(incomingData.usernames)
             break;
     }
 
 }
 
-// When the user submits the form
+// Send Message
+// **This function is fired whenever the user hits enter and a username is set**
+// 1. We create a new JSON object with an event action of new message (which will be caught by the server to send back the content)
+// 2. We send it to the server
+// 3. We clear the input
+
+// **Note that on the server we are not explicitly checking for a "new message" event. We are listening for a "new user" event. If the event isn't that one then it's obviously a "new message" event (I'm currently fasting at the time of writing, this might not make sense at all brah)
 
 const sendMessage = () => {
-    const message = {
+    const message = JSON.stringify({
         action: "new message",         
         body: messageInput.value
     
-    }
-    connection.send(JSON.stringify(message))
+    })
+    connection.send(message)
     messageInput.value = ""
 }
 
-const displayMessage = (message) => {
+// Display Message
+// This function is called when the event sent by the server is "new message"
+// It takes the data sent by the server as a parameter
+// 1. we create a template string containing two span tags wrapped around a <p> tag 
+// 2. we add the values from the data object to the elements
+// 3. we set the innerHTML of the chat container to that string
 
-    const messageElement = document.createElement("P") 
-    const usernameSpan   = document.createElement("span") 
-    const messageBodySpan = document.createElement("span")
+
+const displayMessage = (data) => {
     
-    usernameSpan.classList.add("username")
-    usernameSpan.style.color = message.color
-    usernameSpan.innerHTML = message.user  + ": "
-    
-    messageBodySpan.classList.add("messageBody")
-    messageBodySpan.innerHTML = message.body
-    
-    messageElement.appendChild(usernameSpan)
-    messageElement.appendChild(messageBodySpan)
-    
-    chatContainer.appendChild(messageElement)
+    let messageToDisplay  = `<p> 
+        <span class="username" style="color: ${data.color};"> ${data.user}: </span> 
+        <span class="messageBody"> ${data.body} </span>
+                
+    </p>`
+
+    chatContainer.innerHTML = messageToDisplay
 }
 
 
 // Notify Users
-// 1. Create the element
-// 2. Set its background color
-// 3. Set its text content to the message sent by the server
-// 4. Append the element to the notification container
-// 6. We show the notification container then hide it after two seconds
-// 5. Optional : Play a sound
+// 1. We set the innerHTML of the notificationContainer to the message sent by the server ("user connected, "user left the chat")
+// 2. We set the background color of the container to the one specified in the data object sent by the server
+// 3. We show it (it's display none by default")
+// 4. After two seconds we hide it again
 
 const notifyUsers = (data) => {
     
     const notificationContainer = document.querySelector(".userNotification")
-    const notificationElement = document.createElement("P")
 
-    notificationElement.style.backgroundColor = data.color
-    notificationElement.innerHTML = data.body
-    notificationContainer.appendChild(notificationElement)
+    notificationContainer.innerHTML = "<p>" + data.body  + "</p>"
+    notificationContainer.style.backgroundColor = data.color
     notificationContainer.style.display = "block"
 
     setTimeout( () => notificationContainer.style.display = "none", 2000)
 }
 
-const showConnectedUsers = (usernames) => {
-    
+// Update Connected Users 
+// 1. We loop through the usernames array sent by the server when the event action is "user joined" or "user left" 
+// 2. We create an empty string
+// 3. for each username in the array we append an li element
+// 4. outside the loop we set the innerHTML of the list container to that newly created string
+// 5. When a user leaves this function is fired (it's very fast so people won't noticed that we are not caching the array)
+
+const updateConnectedUsers = (usernames) => {
     const usernamesContainer = document.querySelector(".connectedUsers")
     let   connectedUsers     = ""  
-
-    // loop through the usernames array
-    usernames.forEach( (username) => {
-        
-        // for each one create an li element
-        // and set its text content to the username
-        connectedUsers += "<li>" + username +"</li>"
-    })
-
+    usernames.forEach( (username) => connectedUsers += "<li>" + username +"</li>" )
     usernamesContainer.innerHTML = connectedUsers
 }
 
+
+// Set Username
+// 1. We set the value of username (previously undefined) to the value of the username input 
+// 2. If the username input is not empty (if the user has entered a username)
+//    We send an event to the server with an action of "new user" and a body equal to the newly created username
+//    We then hide the login page And show the chat page
+//    Finally we focus on the chat input for usability
+
 const setUsername = () => {
-    
     // set the value of username to whatever the user has entered in the input field.trim()
     username = usernameInput.value.trim()
 
@@ -121,12 +130,15 @@ const setUsername = () => {
         
 }
 
+// We listen for keydown events 
+// When the user hits enter
+// If when they hit enter, the value of username is defined, then that means the user is trying to send a message (because they can see the chat page)
+    // So in that case we call the sendMessage function
+// However is username is not defined then they are still looking at the login page which means they want to set their username
+    // in that case we call the setUsername() function
+
+
 window.onkeydown = (event) => {
-    
-    // When the user hits enter
-    // If there is a username already sent, that means they already registered, so they want to send a message
-    // If however a username wasn't set, then they are still in the login page trying to enter the chat room
-    // so we set a username then
 
     if (event.which === 13) {
         event.preventDefault()
